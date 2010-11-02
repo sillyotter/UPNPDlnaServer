07b4387618224db253dd54e8f4c2cf01004f88a1
Guy Oliver <guy.oliver@gmail.com>
2010-11-02 16:55:07 -0400

    cleanup

--- src/MediaServer/Web/UpnpWebServer.cs
+++ src/MediaServer/Web/UpnpWebServer.cs
@@ -4,7 +4,7 @@ namespace MediaServer.Web
 {
 	class UpnpWebServer : WebServer
 	{
-		//private readonly ConnectionManager _connectionManager = new ConnectionManager();
+		private readonly ConnectionManager _connectionManager = new ConnectionManager();
 		private readonly ContentDirectory _contentDirectory = new ContentDirectory();
 		private readonly StaticFileHandler _staticFileHandler = new StaticFileHandler();
 		private readonly MusicImageHandler _musicImageHandler = new MusicImageHandler();
@@ -14,7 +14,7 @@ namespace MediaServer.Web
 
 		public UpnpWebServer()
 		{
-			//RequestHandlers.Add(new KeyValuePair<string, BaseRequestHandler>("/MediaServer/Connection/Control", _connectionManager));
+			RequestHandlers.Add(new KeyValuePair<string, BaseRequestHandler>("/MediaServer/Connection/Control", _connectionManager));
 			RequestHandlers.Add(new KeyValuePair<string, BaseRequestHandler>("/MediaServer/Content/Control", _contentDirectory));
 			RequestHandlers.Add(new KeyValuePair<string, BaseRequestHandler>("/MediaServer/GetMusicImage.*", _musicImageHandler));
 			//RequestHandlers.Add(new KeyValuePair<string, BaseRequestHandler>("/MediaServer/GetImageThumbnail.*", _imageThumbnailHandler));
