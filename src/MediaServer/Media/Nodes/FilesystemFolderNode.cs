e3d5a59ce4c7c1e5e7e4dca611df786ebfe41fc5
Guy Oliver <guy.oliver@gmail.com>
2011-03-08 07:34:58 -0500

    remove . named directories

--- src/MediaServer/Media/Nodes/FilesystemFolderNode.cs
+++ src/MediaServer/Media/Nodes/FilesystemFolderNode.cs
@@ -69,7 +69,7 @@ namespace MediaServer.Media.Nodes
 
 			var dirs =
 				from item in Directory.GetDirectories(_location)
-				where !item.StartsWith(".")
+				where !Path.GetFileName(item).StartsWith(".")
 				select new FilesystemFolderNode(this, Path.GetFileName(item), item);
 			
 			var mediaFiles =
