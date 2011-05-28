1e0c3f9541d9771be96f8fd8eb6070ee2d7a6c56
Guy Oliver <guy.oliver@gmail.com>
2011-05-28 11:28:50 -0400

    new encode method

--- src/MediaServer/Media/Nodes/ResourceNode.cs
+++ src/MediaServer/Media/Nodes/ResourceNode.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Net;
 using System.Web;
 using System.Xml.Linq;
-using System.Text.RegularExpressions;
 
 namespace MediaServer.Media.Nodes
 {
@@ -29,7 +28,7 @@ namespace MediaServer.Media.Nodes
 					new XAttribute("parentID", ParentId),
 					new XAttribute("childCount", 0),
 					new XAttribute("restricted", true),
-					new XElement(Dc + "title", new XText(Regex.Escape(Title))),
+					new XElement(Dc + "title", new XText(HttpUtility.HtmlEncode(Title))),
 					new XElement(Upnp + "class", new XText(Class)),
 					new XElement(
 						Upnp + "albumArtURI",
